5048520d5a2fa524a768e089da2b587b5e57febe
Stenio Junior <stenioloureiro@gmail.com>
2020-09-10 00:18:58 -0300

    ajudar diretorios arquivos terraform

--- eip.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_eip" "nat" {
-  instance = aws_instance.privapp.id
-}
--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo docker run --name docker-nginx -p 80:80 -d nginx
